887d00cec1586b9473ebc5af5d8466a71837aec2
emonzon42 <elijah.monzon42@gmail.com>
2022-04-12 17:54:51 -0400

    created product page

---
+++ Final Project/pages/product.php
@@ -0,0 +1,46 @@
+<!-- © 2022, Elijah C. Monzon Alvarenga. -->
+<!doctype html>
+<html lang="en">
+
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title>Happy Feets | Product Name</title>
+        <link rel="shortcut icon" type="image/jpg" href="../img/Smileyy.png"/>
+        <link rel="icon" type="image/jpg" href="../img/Smileyy.png"/>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="author" content="Elijah Alvarenga">
+        <link rel="stylesheet" href="../css/style.css"> 
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+        
+    </head>
+
+    <body>
+        <div id="header"></div>
+    
+        <!-- Page Specific-->
+        <div class="ibox"></div>
+    
+        <div class="box">
+            <img src="https://via.placeholder.com/500" alt="placeholder">
+            
+            <aside>
+                <h1>Product Name</h1>
+                other stuff
+            </aside>
+            
+    
+        </div>
+        <div class="ibox"></div>
+    
+    
+        <!-- Footer-->
+        <div id="footer"></div>
+        
+        <script src="../js/loadheadfoot.js"></script>
+    </body>
+
+
+
+</html>
