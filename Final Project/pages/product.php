36897b10ea4ff6869fe1863c8705cae4ac4a0d25
emonzon42 <elijah.monzon42@gmail.com>
2022-04-25 17:31:46 -0400

    product page loads depending on product in db
    i still have to test this but the file server is down rn :/

--- Final Project/pages/product.php
+++ Final Project/pages/product.php
@@ -1,4 +1,46 @@
 <!-- © 2022, Elijah C. Monzon Alvarenga. -->
+
+<?php
+    if (isset($_GET['product'])) {
+        if(empty($_GET['name']) || empty($_GET['color1']) || empty($_GET['hashid'])){ //the hashid = id * 42 + (15^4)
+            echo "Please provide any missing values";
+        } else{
+            if(is_numeric($_GET['hashid'])){
+                require_once './login/connection.php';
+
+                $id = ($_GET['hashid'] - (15^4))/42;
+                
+                $query = "SELECT * FROM sneakers WHERE id=".$id;
+                $result = $conn->query($query);
+                if (!$result) {
+                    $message =  "Error: " . $sql . "<br>" . $conn->error;
+                }
+                $row = mysqli_fetch_row($result);
+
+                $name = $row[1];
+                $alt = $row[2];
+                $size = $row[3];
+                $c1 = $row[4];
+                $c2 = $row[5];
+                $c3 = $row[6];
+                $price = $row[7];
+                $itemdesc = $row[8];
+                $qty = $row[9];
+                $img = $row[10];
+
+
+                $conn->close(); 
+            } else{
+                echo "Error Please go back and try again.";
+            }
+        }
+
+    } else{
+        header('HTTP/1.0 404');
+        echo "Page not found.";
+    }
+?>
+
 <!doctype html>
 <html lang="en">
 
@@ -25,18 +67,24 @@
         <div class="box">
 
             <div class="left pinfo">
-                <h1>Product Name</h1>
-                <p>$XX.XX</p>
-
-                <p>Size: XX.X</p>
-
+                <?php 
+                echo '<h1>'.$name.'</h1>';
+                echo '<h3>'.$alt.'</h3>';
+                echo '<p>$'.$price.'</p>';
+                echo '<p>Size: '.$size.'</p>';
+                ?>
+                
                 <button id="buy" class= "bigbutton">Buy Now</button>
             </div>
             <div class="right">
-            <img src="https://via.placeholder.com/500" alt="placeholder">
+                <?php 
+                    echo '<img src="'.$img.'" alt="'.$name.' '.$alt.'">;'
+                ?>
             </div>
             <p id="desc">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores ratione quas, quidem adipisci voluptatibus quia facere blanditiis hic at mollitia necessitatibus repellendus itaque fugiat, natus consequuntur deleniti a qui magni.
+                <?php 
+                    echo $itemdesc;
+                ?>
             </p>
         </div>
         <div class="ibox"></div>
