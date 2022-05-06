4f42d28ec04cd03aed66a1349172160aa73f78e3
emonzon42 <elijah.monzon42@gmail.com>
2022-05-05 20:15:00 -0400

    changed format of 3column on home page

--- Final Project/pages/index.php
+++ Final Project/pages/index.php
@@ -1,4 +1,14 @@
 <!-- © 2022, Elijah C. Monzon Alvarenga. -->
+<?php
+require_once './login/connection.php';
+
+$query = "SELECT * FROM sneakers;";
+$result = $conn->query($query);
+if (!$result) {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+}
+?>
+
 <!doctype html>
 <html lang="en">
 
@@ -6,22 +16,22 @@
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <title>Happy Feets | Homepage</title>
-        <link rel="shortcut icon" type="image/jpg" href="../img/Smileyy.png"/>
-        <link rel="icon" type="image/jpg" href="../img/Smileyy.png"/>
+        <link rel="shortcut icon" type="image/jpg" href="../img/Smileyy.png" />
+        <link rel="icon" type="image/jpg" href="../img/Smileyy.png" />
         <meta name="description" content="">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="author" content="Elijah Alvarenga">
-        <link rel="stylesheet" href="../css/style.css"> 
+        <link rel="stylesheet" href="../css/style.css">
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
-        
+
     </head>
 
     <body>
         <div id="header"></div>
-    
+
         <!-- Page Specific-->
         <div id="homehead">
-    
+
             <img src="../img/homecover2.jpg" max-width="2160px" max-height="1080px" alt="dummy image" id="homecov">
             <h1 class="cen">Happiness Is Where The Heart Is</h1>
             <a href="shop">Shop All</a>
@@ -29,23 +39,31 @@
         <div class="ibox"></div>
         <div class="box">
             <p>Your Happiness Starts Here. Let us at Happy Feets take care of you and make sure your experience is a great one.
-            From our top-notch customer service, to our quality products listed below market prices, there is nowhere else that
-            you will be able to have an experience like this one.</p>
+                From our top-notch customer service, to our quality products listed below market prices, there is nowhere else that
+                you will be able to have an experience like this one.</p>
         </div>
         <div class="ibox"></div>
         <div class="box">
-            <ul>
-                <li><img src="https://via.placeholder.com/300" alt="dummy image"></li>
-                <li><img src="https://via.placeholder.com/300" alt="dummy image"></li>
-                <li><img src="https://via.placeholder.com/300" alt="dummy image"></li>
-            </ul>
+            <h2 class="center">Newest Releases</h2>
+            <div class="three-col">
+                <img src="https://via.placeholder.com/300" alt="dummy image">
+                <p>Product Name</p>
+            </div>
+            <div class="three-col">
+                <img src="https://via.placeholder.com/300" alt="dummy image">
+                <p>Product Name</p>
+            </div>
+            <div class="three-col">
+                <img src="https://via.placeholder.com/300" alt="dummy image">
+                <p>Product Name</p>
+            </div>
         </div>
         <div class="ibox"></div>
-    
-    
+
+
         <!-- Footer-->
         <div id="footer"></div>
-        
+
         <script src="../js/loadheadfoot.js"></script>
     </body>
 
